86a154e6266373c778cd896a89170cb83c851946
Prateek Singh <prateeksingh3102000@gmail.com>
2024-11-18 22:53:47 +0530

    Added new Patterns

--- Pattern_1.java
+++ Pattern_1.java
@@ -4,10 +4,66 @@ public class Pattern_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the no. of rows: ");
-        int rows = sc.nextInt();
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        int num = sc.nextInt();
+
+        /* Pattern
+
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+
+         */
+//        for (int i = 1; i <= num; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        /* Pattern
+        1
+        12
+        123
+        1234
+        12345
+        */
+//        for (int row = 1; row <= num; row++) {
+//            for (int col = 1; col <= row ; col++) {
+//                System.out.print(col);
+//            }
+//            System.out.println();
+//        }
+
+        /* Pattern
+        1
+        2 3
+        4 5 6
+        7 8 9 10
+        11 12 13 14 15
+        */
+//        int start = 1;
+//        for (int row = 1; row <= num; row++) {
+//            for (int col = 1; col <= row ; col++) {
+//                System.out.print(start+" ");
+//                start++;
+//            }
+//            System.out.println();
+//        }
+
+        /* Pattern
+        A
+        B C
+        D E F
+        G H I J
+        K L M N O
+        */
+        char ch ='A';
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= row ; col++) {
+                System.out.print(ch+" ");
+                ch++;
             }
             System.out.println();
         }
--- Pattern_2.java
+++ Pattern_2.java
@@ -3,7 +3,7 @@ public class Pattern_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
-        int rows = sc.nextInt();
+        int num = sc.nextInt();
 
         /* Pattern
 
@@ -15,13 +15,25 @@ public class Pattern_2 {
 
          */
 
-        for (int i = rows; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+//        for (int i = num; i >= 1; i--) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        /* Pattern
+            55555
+            4444
+            333
+            22
+            1
+        */
+        for (int row = num; row >= 1; row--) {
+            for (int col = 0; col < row; col++) {
+                System.out.print(row);
             }
             System.out.println();
         }
-
-        /* Pattern */
     }
 }
