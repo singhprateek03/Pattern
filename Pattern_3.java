55cd9bf8d191d9651fbc4a590a260d53cb86374e
Prateek Singh <prateeksingh3102000@gmail.com>
2024-11-19 22:57:15 +0530

    New Patterns

--- Pattern_3.java
+++ Pattern_3.java
@@ -6,9 +6,15 @@ public class Pattern_3 {
         System.out.print("Enter the number of rows: ");
         int num = sc.nextInt();
         StarLeftTriangle(num);
-
     }
     public static void StarLeftTriangle(int num){
+        /* Pattern
+             *
+            **
+           ***
+          ****
+         *****
+        */
 //        for (int i = 1; i <= num; i++) {
 //            // For printing space
 //            for (int j = (num-i); j > 0 ; j--) {
@@ -20,15 +26,118 @@ public class Pattern_3 {
 //            }
 //            System.out.println();
 //        }
+/*      =================================================== */
+//        for (int row = 1; row <= num ; row++) {
+//            for (int col = 1; col <= num-row; col++) {
+//                System.out.print(" ");
+//            }
+//            for (int col = 1; col <= row ; col++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
+        /* Pattern
+          *
+         * *
+        * * *
+       * * * *
+      * * * * *
+         */
+//        for (int row = 1; row <= num ; row++) {
+//            for (int col = 1; col <= num-row; col++) {
+//                System.out.print(" ");
+//            }
+//            for (int col = 1; col <= row ; col++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        /* with using 2 for loop */
+//        for (int row = 0; row < num ; row++) {
+//            for (int col = 0; col <= num; col++) {
+//                if (col < num-row)
+//                    System.out.print(" ");
+//                else
+//                    System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+        
+        /* Pattern
+        * * * * *
+         * * * *
+          * * *
+           * *
+            *
+         */
+//        for (int row = 0; row < num; row++) {
+//            for (int col = 0; col < num; col++)
+//                if (col < row)
+//                    System.out.print(" ");
+//                else
+//                    System.out.print("* ");
+//            System.out.println();
+//        }
+        
+        /* Pattern
+        1 1 1 1 1
+         2 2 2 2
+          3 3 3
+           4 4
+            5
+
+         */
+//        for (int row = 1; row <=num ; row++) {
+//            for (int col = 1; col <= num ; col++)
+//                if (col<row)
+//                    System.out.print(" ");
+//                else
+//                    System.out.print(row+" ");
+//            System.out.println();
+//        }
+
+        /* Pattern
+        1 2 3 4 5
+         2 3 4 5
+          3 4 5
+           4 5
+            5
+         */
+//        for (int row = 1; row <=num ; row++) {
+//            for (int col = 1; col <= num ; col++)
+//                if (col< row)
+//                    System.out.print(" ");
+//                else
+//                    System.out.print(col+" ");
+//            System.out.println();
+//        }
+        /* Pattern
+          1
+         123
+        12345
+         123
+          1
+         */
+        int spaces = num/2;
+        int star = 1;
         for (int row = 1; row <= num ; row++) {
-            for (int col = 1; col <= num-row; col++) {
+            for (int col = 1; col <= spaces ; col++) {
                 System.out.print(" ");
             }
-            for (int col = 1; col <= row ; col++) {
-                System.out.print("*");
+            for (int col = 1; col <= star; col++) {
+                System.out.print(col);
             }
             System.out.println();
+            if (row <= num/2) {
+                spaces--;
+                star += 2;
+            }
+            else {
+                spaces++;
+                star -= 2;
+            }
         }
     }
 }
