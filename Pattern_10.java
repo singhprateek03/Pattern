36405c7b7da6b2c918cc546e7c9914bc3f6203bc
Prateek Singh <prateeksingh3102000@gmail.com>
2024-11-19 20:48:10 +0530

    Added new pattern

--- Pattern_10.java
+++ Pattern_10.java
@@ -5,13 +5,27 @@ public class Pattern_10 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int num = sc.nextInt();
-        // This loop handles both upper and lower part
-        for (int row = 0; row < 2*num; row++) {
+//        // This loop handles both upper and lower part
+//        for (int row = 0; row < 2*num; row++) {
+//
+//            // This loop handles stars based on rows
+//            int column = row > num ? 2 * num - row: row;
+//
+//            for (int col = 1; col < column; col++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
 
-            // This loop handles stars based on rows
-            int column = row > num ? 2 * num - row: row;
+        for (int row = 1; row <= num ; row++) {
+            for (int col = 1; col <= row; col++) {
+                    System.out.print("* ");
+            }
+            System.out.println();
+        }
 
-            for (int col = 1; col < column; col++) {
+        for (int i = num-1; i >= 1 ; i--) {
+            for (int j = 1; j <= i ; j++) {
                 System.out.print("* ");
             }
             System.out.println();
